55499fa7c7fb9e635fa6fac3dbca71e93059ba4c
Salah-Eddine <31400rng@gmail.com>
2024-01-26 19:59:48 +0100

    Initial commit: Project init

---
+++ fonctions_projet_python.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 18:34:26 2024
+
+@author: i50031073
+"""
+
+import numpy as np
+import pandas as pd
+import os
+
+# Charger les données depuis le fichier CSV
+fichier_xlsx = "male_players_triés.xlsx"  
+data = pd.read_excel(fichier_xlsx)
+
+"Le but de ces fonctions est de déterminer le meilleur joueur dans chaque catégorie et dans chaque position"
+
+
+# Caractéristiques attribuées à chaque bloc
+
+caracteristiques_par_blocs = {
+    'Gardien': ['Jumping','Defending','Standing','Physicality'],
+    'Defense': ['Defending', 'Heading', 'Strength', 'Standing Tackle', 'Stamina',  'Strength', 'Sliding', 'Standing','Physicality'], 
+    'Milieu': ['Interceptions','Vision', 'Dribbling', 'Passing', 'Pace', 'Crossing', 'Stamina','Agression','Agility'], 
+    'Attaque': ['Positioning','Penalties','Vision', 'Agility', 'Aggression','Dribbling', 'Passing', 'Shooting','Acceleration', 'Finishing']
+}
+
+
+# Fonction pour trouver  les 100 meilleurs joueurs par blocs et générer un parmi eux de manière aléatoire
+
+        
+def generer_top_joueurs(data, categorie, blocs, caracteristiques_par_blocs, n_top=100):
+    categorie_data = data[data['Catégorie'] == categorie]
+    blocs_data = categorie_data[categorie_data['Blocs'] == blocs]
+    caracteristiques = caracteristiques_par_blocs[blocs]
+
+    # Ajouter une colonne 'Score' en calculant la somme des caractéristiques pour chaque joueur
+    blocs_data['Score'] = blocs_data[caracteristiques].sum(axis=1)
+
+    # Générer une liste aléatoire des indices des 100 meilleurs joueurs
+    top_indices = blocs_data.nlargest(n_top, 'Score').index.tolist()
+
+    # Sélectionner un indice aléatoire parmi les 100 meilleurs joueurs
+    indice_aleatoire = np.random.choice(top_indices)
+
+    # Récupérer le nom du joueur correspondant à l'indice aléatoire
+    nom_joueur_aleatoire = blocs_data.loc[indice_aleatoire, 'Name']
+
+    return nom_joueur_aleatoire
+
+
+
+
+
---
+++ projet python.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  7 17:33:09 2024
+
+@author: i50031073
+"""
+
+import pandas as pd
+import os
+from fonctions_projet_python import * #lancer la première partie du code avant
+
+# Charger les données depuis le fichier Excel
+fichier_xlsx = "male_players.xlsx"  
+data = pd.read_excel(fichier_xlsx)
+
+# Définir les points de coupure pour répartir également les joueurs
+points_de_coupe = [0, data['Overall'].quantile(0.25), data['Overall'].quantile(0.5),
+                   data['Overall'].quantile(0.75), float('inf')]
+
+# Classer les joueurs en fonction de leur classement général
+data['Catégorie'] = pd.cut(data['Overall'], bins=points_de_coupe,
+                           labels=['Facile', 'Moyen', 'Difficile', 'Expert'], right=False)
+
+# Sauvegarder le résultat dans un nouveau fichier Excel
+nouveau_fichier_excel = "male_players_triés.xlsx"  # Remplacez cela par le chemin réel de votre nouveau fichier Excel
+data.to_excel(nouveau_fichier_excel, index=False)
+
+print("Les joueurs ont été classés avec succès.")
+
+# Charger les données depuis le fichier CSV
+fichier_xlsx = "male_players_triés.xlsx"  
+data = pd.read_excel(fichier_xlsx)
+
+# Trier les positions en fonction des blocs
+
+bloc_gardien = ['GK']
+bloc_defensif = ['LH', 'SW','RH','RB','LB','LCB','CB','CH','RCB','LWB','RWB','WB']
+bloc_milieu = ['DM','CDM','LW','LM','CAM','CM','M','RM','RW','AMR','WF','AML','AMC','AM']
+bloc_offensif = ['LS','RS','LF','SS','RF','ST','CF']
+
+# Ajouter une colonne 'Blocs' en fonction de la position
+
+data['Blocs'] = data['Position'].apply(lambda x: 
+                                        'Defense' if x in bloc_defensif 
+                                        else ('Milieu' if x in bloc_milieu 
+                                              else ('Attaque' if x in bloc_offensif
+                                                    else 'Gardien')))
+
+
+data_sorted = data.sort_values(by=['Blocs', 'Position'])
+
+# Enregistrer la DataFrame triée
+data.to_excel("male_players_triés.xlsx", index=False)
+
+print("Les joueurs ont été triés par blocs.")
+
+
+# Test de la fonction qui donne un nom aléatoire d'un des 100 meilleurs joueurs en fonction de la catégorie et du bloc
+caracteristiques_par_blocs = {
+    'Gardien': ['Jumping','Defending','Standing','Physicality'],
+    'Defense': ['Defending', 'Heading', 'Strength', 'Stamina', 'Jumping', 'Strength', 'Sliding', 'Standing', 'Physicality'],
+    'Milieu': ['Interceptions', 'Vision', 'Dribbling', 'Passing', 'Pace', 'Crossing', 'Stamina', 'Aggression', 'Agility'],
+    'Attaque': ['Positioning', 'Penalties', 'Vision', 'Agility', 'Aggression', 'Dribbling', 'Passing', 'Shooting', 'Acceleration', 'Finishing']
+}
+
+
+# 'Facile', 'Moyen', 'Difficile', 'Expert' sont les catégories possibles
+# 'Gardien', 'Defense', 'Milieu' et 'Attaque' sont les blocs possibles
+
+nom_joueur_aleatoire = generer_top_joueurs(data, 'Expert', 'Gardien', caracteristiques_par_blocs)
+
+# Afficher le nom du joueur aléatoire
+print(f"Nom du joueur aléatoire - Catégorie: Expert, Bloc: Gardien: {nom_joueur_aleatoire}")
+
+
+
+
+        
